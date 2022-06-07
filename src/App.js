614b07dce298fcd7e28638b032eb2d3050bbdf13
ibimina <96787210+ibimina@users.noreply.github.com>
2022-06-07 21:22:04 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Header extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  state = {};
+  render() {
+    return (
+      <>
+        <h1>{this.props.intro}</h1>
+        <h2>{this.props.title}</h2>
+      </>
+    );
+  }
+}
+
+class Main extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  state = {};
+  render() {
+    return (
+      <>
+        <ul>{this.props.evenOddPrime()}</ul>
+      </>
+    );
+  }
+}
+
+class App extends React.Component {
+  // state = {  }
+
+  evenOddPrime = () => {
+    let ne = [];
+    let rate = 33;
+
+    for (let i = 0; i <= rate; i++) {
+      let flag = true;
+      for (let m = 2; m < i - 1; m++) {
+        // if (i === 0 && i === 1) {
+        //   flag = true;
+        // }
+        if (i % m === 0) {
+          flag = false;
+          break;
+        }
+      }
+      if (i % 2 === 0 && !flag) {
+        ne.push(
+          <li key={i} style={{ backgroundColor: "green" }}>
+            {i}
+          </li>
+        );
+      } else if (!i % 2 === 0 && !flag) {
+        ne.push(
+          <li key={i} style={{ backgroundColor: "red" }}>
+            {i}
+          </li>
+        );
+      } else if (flag) {
+        ne.push(
+          <li key={i} style={{ backgroundColor: "yellow" }}>
+            {i}
+          </li>
+        );
+      }
+    }
+    return ne;
+  };
+  render() {
+    let intro = "30days of React";
+    let title = "Number generator";
+
+    return (
+      <div className="wrapper">
+        <Header intro={intro} title={title} />
+        <Main evenOddPrime={this.evenOddPrime} />
+      </div>
+    );
+  }
 }
 
 export default App;
